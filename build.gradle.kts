659eda4c5e60eac1fc32ec619071a47df1f213bb
joe <burtonjae@hotmail.co.uk>
2025-01-26 19:41:37 +0000

    Add fuzz test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.code-intelligence:jazzer-junit:0.22.1")
 }
 
 tasks.test {
---
+++ src/test/java/net/earthcomputer/unpickv3parser/reader/TestFuzz.java
@@ -0,0 +1,25 @@
+package net.earthcomputer.unpickv3parser.reader;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.junit.FuzzTest;
+import net.earthcomputer.unpickv3parser.UnpickParseException;
+import net.earthcomputer.unpickv3parser.UnpickV3Reader;
+import net.earthcomputer.unpickv3parser.tree.UnpickV3Visitor;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public final class TestFuzz {
+    @EnabledIfSystemProperty(named = "unpick.fuzzReader", matches = "true")
+    @FuzzTest
+    public void fuzzReader(FuzzedDataProvider data) throws IOException {
+        String unpickFile = data.consumeRemainingAsString();
+        try (UnpickV3Reader reader = new UnpickV3Reader(new StringReader(unpickFile))) {
+            reader.accept(new UnpickV3Visitor() {
+            });
+        } catch (UnpickParseException ignore) {
+            // we're trying to catch crashes other than syntax errors
+        }
+    }
+}
